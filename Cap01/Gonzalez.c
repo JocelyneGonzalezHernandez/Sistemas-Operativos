a07205a843ee13574ec221c6f404316ee76d587f
JocelyneGonzalezHernandez <128755176+JocelyneGonzalezHernandez@users.noreply.github.com>
2024-07-27 14:40:54 -0600

    Create Gonzalez.c
    Programación en C pt.1 : Hola Mundo

---
+++ Cap01/Gonzalez.c
@@ -0,0 +1,14 @@
+/***************************************************
+Autor: Jocelyne Gonzalez Hernández.
+Fecha: 23/07/2024
+Materia: Sistemas Operativos
+Tema: Programación en C: Primera parte
+***************************************************/
+#include <stdio.h>  //
+
+int main( int argc, char *argv[]) {
+
+  printf ("Hola mundo\n");
+  printf("\t\t Bienvenidos \n");
+  return 0;
+}
